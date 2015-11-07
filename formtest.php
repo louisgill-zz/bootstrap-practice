ba7c5849e30559fa31150c76fd1133cdef1b6357
Louis Gill <lgill7@cnm.edu>
2015-11-07 12:09:32 -0700

    formtest.php

---
+++ formtest.php
@@ -0,0 +1,16 @@
+<?php // formtest.php
+	echo <<<END
+	<html lang="en">
+		<head>
+			<title>Form Test</title>
+		</head>
+		<body>
+		<form method="post" action="formtest.php">
+			What is your name?
+			<input type="text" name="name">
+			<input type="submit">
+		</form>
+		</body>
+	</html>
+_END;
+?>
